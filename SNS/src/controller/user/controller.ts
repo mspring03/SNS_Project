45501e5c33a94acaab8da41ec05ae29409965ffb
yu myeong cheol <mspringmspring@naver.com>
2020-06-27 17:15:43 +0900

    [MODIFY] <user/controller.ts> function modification

--- SNS/src/controller/user/controller.ts
+++ SNS/src/controller/user/controller.ts
@@ -1,33 +1,34 @@
 import { Request, Response } from "express";
-import User from "./query";
-import token from "./mkToken";
+import * as userQuery from "./query";
+import * as token from "./mkToken";
 
-const mkToken = token.mktoken;
+const mktoken = token.mktoken;
 
 const signUpUser = async (req: Request, res: Response) => {
-    const { userId, password, name, introduce } = req.body;
+    const { userName, password, nickName } = req.body;
     
-    if (await User.findUserById(userId)) 
+    if (await userQuery.findUserById(userName)) 
         throw new Error('이미 있는 아이디');
 
-    await User.userCreate(userId, password, name, introduce);
+    await userQuery.userCreate(userName, password, nickName);
     res.status(200).json({ message: "회원가입 성공"}).end();
 }
 
 const signInUser = async (req: Request, res: Response): Promise<any> => {
-    const { userId, password } = req.body;
-    const user = await User.findUserById(userId);
-    
+    const { userName, password } = req.body;
+    const user = await userQuery.findUserById(userName);
+
     if (!user) throw new Error("로그인 실패"); 
         
     if(user.password === password) {
-        const token = await mktoken(req, res, user);
-        await User.tokenUpdate(user.userId, token);
+        const token: string = await mktoken(req, res, user);
+        await userQuery.tokenUpdate(user.userName, token);
         res.status(200).json({ message: '로그인 성공', token, }).end();
     }
 }
 
-module.exports = {
+export {
     signUpUser,
-    signInUser
+    signInUser    
 }
+

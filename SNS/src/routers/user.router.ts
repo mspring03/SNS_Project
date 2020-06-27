56054ec17ac32b396e9d3e8624a1cbcadc0ede67
yu myeong cheol <mspringmspring@naver.com>
2020-06-27 17:06:28 +0900

    [ADD] <routers/user.router.ts> add signup, signin router

--- SNS/src/routers/user.router.ts
+++ SNS/src/routers/user.router.ts
@@ -1,6 +1,12 @@
 import express from "express";
-// import userController from "../controller/user/controller.ts";
+import * as userController from "../controller/user/controller";
+import * as middelware from "../middleware/errorHandler"
 
 const router = express.Router();
+const signUpHanddler = middelware.tryCatchMiddleware.NotFound(userController.signUpUser);
+const signInHanddler = middelware.tryCatchMiddleware.NotFound(userController.signInUser);
+
+router.post("/signUp", signUpHanddler);
+router.post("/signIn", signInHanddler);
 
 export default router;

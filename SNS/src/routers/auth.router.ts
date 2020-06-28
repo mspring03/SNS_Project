ad2571937ceb91020e2db6c48f0e4e4057e537e3
yu myeong cheol <mspringmspring@naver.com>
2020-06-28 21:54:48 +0900

    [MODIFY] <routers/auth.router.ts> Modify file paths and add routers

--- SNS/src/routers/auth.router.ts
+++ SNS/src/routers/auth.router.ts
@@ -1,12 +1,17 @@
 import express from "express";
-import * as userController from "../controller/user/controller";
+import * as userController from "../controller/auth/controller";
 import * as middelware from "../middleware/errorHandler"
+import * as authMiddleware from "../middleware/jwt";
 
 const router = express.Router();
 const signUpHanddler = middelware.tryCatchMiddleware.NotFound(userController.signUpUser);
 const signInHanddler = middelware.tryCatchMiddleware.NotFound(userController.signInUser);
+const refreshHanddler = middelware.tryCatchMiddleware.NotFound(userController.refresh)
+const eccessToken = authMiddleware.eccessToken;
+const refreshToken = authMiddleware.refreshToken;
 
 router.post("/signUp", signUpHanddler);
 router.post("/signIn", signInHanddler);
+router.get("/refresh", refreshToken, refreshHanddler);
 
 export default router;

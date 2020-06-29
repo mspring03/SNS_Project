589656063d4a3c790a1cd502f3aab1bcd58662d0
yu myeong cheol <mspringmspring@naver.com>
2020-06-29 22:45:28 +0900

    [MODIFY] <auth/controller.ts> auth function refactoring

--- SNS/src/controller/auth/controller.ts
+++ SNS/src/controller/auth/controller.ts
@@ -5,44 +5,40 @@ import * as token from "./mkToken";
 const mktoken = token.mktoken;
 const mkRefreshtoken = token.mkRefreshtoken;
 
-const signUpUser = async (req: Request, res: Response) => {
-    const { userName, password, nickName } = req.body;
-    
-    if (await userQuery.findUserById(userName)) 
-        throw new Error('이미 있는 아이디');
-
-    await userQuery.userCreate(userName, password, nickName);
-    res.status(200).json({ message: "회원가입 성공"}).end();
-}
-
 const signInUser = async (req: Request, res: Response) => {
     const { userName, password } = req.body;
-    const user = await userQuery.findUserById(userName);
+    const user: object = await userQuery.findUserById(userName);
 
-    if (!user) throw new Error("로그인 실패"); 
-        
-    if(user.password === password) {
+    if (!user) throw new Error('signIn faild'); 
+
+    if(user['password'] === password) {
         const token: string = await mktoken(req, user);
         const refreshtoken: string = await mkRefreshtoken(req, user);
-        await userQuery.tokenUpdate(user.userName, token, refreshtoken);
-        res.status(200).json({ message: '로그인 성공', token, refreshtoken }).end();
+        await userQuery.tokenUpdate(user['userName'], token, refreshtoken);
+        res.status(200).json({ message: 'signIn success', token, refreshtoken }).end();
     }
 }
 
-const refresh = async (req: Request, res: Response) => {
-    const eccesstoken = req.header['x-eccess-token'];
-    const refreshtoken = req.header['x-refresh-token'];
-    const user = await userQuery.findUserByRefreshToken(refreshtoken);
+const me = async (req: Request, res: Response) => {
+    const eccesstoken: string = req.header['x-eccess-token'];
+    const user: object = await userQuery.findUserByToken(eccesstoken);
+    res.status(200).json({ message: 'user return', user });
+}
 
-    if(user.token === eccesstoken) {
+const refresh = async (req: Request, res: Response) => {
+    const eccesstoken: string = req.header['x-eccess-token'];
+    const refreshtoken: string  = req.header['x-refresh-token'];
+    const user: object = await userQuery.findUserByRefreshToken(refreshtoken);
+
+    if(user['token'] === eccesstoken) {
         const token: string = await mktoken(req, user);
-        await userQuery.tokenUpdate(user.userName, token, refreshtoken);
-        res.status(200).json({ message: '토큰 재발급', token, refreshtoken }).end();
+        await userQuery.tokenUpdate(user['userName'], token, refreshtoken);
+        res.status(200).json({ message: 'token refresh', token, refreshtoken }).end();
     }
 } 
 
 export {
-    signUpUser,
+    me,
     signInUser,
     refresh 
 }

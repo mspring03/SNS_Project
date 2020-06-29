14902d2079c9716fd9e9adc74f7ddd7e69f559df
yu myeong cheol <mspringmspring@naver.com>
2020-06-29 21:56:44 +0900

    [ADD] <routers/auth.router.ts> add me router, delete signup router

--- SNS/src/routers/auth.router.ts
+++ SNS/src/routers/auth.router.ts
@@ -4,14 +4,15 @@ import * as middelware from "../middleware/errorHandler"
 import * as authMiddleware from "../middleware/jwt";
 
 const router = express.Router();
-const signUpHanddler = middelware.tryCatchMiddleware.NotFound(userController.signUpUser);
 const signInHanddler = middelware.tryCatchMiddleware.NotFound(userController.signInUser);
+const meHanddler = middelware.tryCatchMiddleware.NotFound(userController.me);
 const refreshHanddler = middelware.tryCatchMiddleware.NotFound(userController.refresh)
-const eccessToken = authMiddleware.eccessToken;
+
+const accessToken = authMiddleware.accessToken;
 const refreshToken = authMiddleware.refreshToken;
 
-router.post("/signUp", signUpHanddler);
 router.post("/signIn", signInHanddler);
+router.get("/me", accessToken, meHanddler);
 router.get("/refresh", refreshToken, refreshHanddler);
 
 export default router;

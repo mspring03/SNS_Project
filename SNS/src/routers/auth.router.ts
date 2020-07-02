cc2616c3e3803a42aad737d6d975a27d82038e5c
yu myeong cheol <mspringmspring@naver.com>
2020-07-02 22:38:01 +0900

    [ADD] <router/auth.router.ts> Router to auto through the GitHub

--- SNS/src/routers/auth.router.ts
+++ SNS/src/routers/auth.router.ts
@@ -3,6 +3,8 @@ import * as userController from "../controller/auth/controller";
 import * as middelware from "../middleware/errorHandler"
 import * as authMiddleware from "../middleware/jwt";
 
+const passportGitHub = require('../auth/github');
+
 const router = express.Router();
 const signInHanddler = middelware.tryCatchMiddleware.NotFound(userController.signInUser);
 const meHanddler = middelware.tryCatchMiddleware.NotFound(userController.me);
@@ -14,5 +16,11 @@ const refreshToken = authMiddleware.refreshToken;
 router.post("/signIn", signInHanddler);
 router.get("/me", accessToken, meHanddler);
 router.get("/refresh", refreshToken, refreshHanddler);
+router.get('/github', passportGitHub.authenticate('github'));
+router.get('/github/callback', passportGitHub.authenticate('github', { failureRedirect: '/signIn' }),
+  function(req, res) {
+
+    res.redirect('/');
+});
 
 export default router;

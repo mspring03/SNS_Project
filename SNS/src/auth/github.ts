f226bf474647967ec21dc61f7731ea96e7ecd47a
yu myeong cheol <mspringmspring@naver.com>
2020-07-03 23:26:21 +0900

    [ADD] <auth/github.ts> user create code

--- SNS/src/auth/github.ts
+++ SNS/src/auth/github.ts
@@ -2,20 +2,36 @@ import GitHub from "passport-github";
 import passport from "passport";
 import path from 'path';
 import * as dotenv from "dotenv";
-// import { User } from "../models/user";
+import { User } from "../models/user";
 
 const GitHubStrategy = GitHub.Strategy
 dotenv.config({ path: path.join(__dirname, '../../.env') });
 
 passport.use(new GitHubStrategy({
-    clientID: "",
-    clientSecret: "",
-    callbackURL: "http://10.156.145.143:3000/api/auth/github/callback"
-  },
+    clientID: "045649b62fdd736a67fe",
+    clientSecret: "d31d36e0d932281bb211020f936670813705c11d",
+    callbackURL: "http://10.156.145.143:8080/api/auth/github/callback",
+    scope: [ 'user:email' ],
+}, async function(accessToken, refreshToken, profile, cb) {
+    if(!await User.findOne({ where: { email: profile.emails[0].value }})){
+        const user = await User.create({ 
+            email: profile.emails[0].value,
+            password: `${profile._json.id}${profile._json.node_id}`,
+            nickName: profile._json.login
+         });
+        
+    }
 
-  function(accessToken, refreshToken, profile, cb) {
-
+    return cb(null, profile);
   }
 ));
+
+passport.serializeUser(function(user, cb) {
+    cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+    cb(null, obj);
+});
 
 module.exports = passport;

38694ea8d32c26e268ca930128e7d6d6c5297712
yu myeong cheol <mspringmspring@naver.com>
2020-07-06 23:18:50 +0900

    [CREATE] <user/nodemailer.ts> nodemailer

---
+++ SNS/src/controller/user/nodemailer.ts
@@ -0,0 +1,33 @@
+const nodemailer = require('nodemailer');
+
+export const mailSender = {
+    sendGmail : function(param){
+        const transporter = nodemailer.createTransport({
+            service: 'gmail'
+            ,prot : 587
+            ,host :'smtp.gmlail.com'
+            ,secure : false
+            ,requireTLS : true
+            , auth: {
+              user: 'aespringaa@gmail.com'
+              ,pass: 'aa030517'
+            }
+        });
+        // 메일 옵션
+        const mailOptions = {
+                from: 'aespringaa@gmail.com',
+                to: param.toEmail, // 수신할 이메일
+                subject: param.subject, // 메일 제목
+                text: param.text // 메일 내용
+            };
+
+        transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Email sent: ' + info.response);
+            }
+        });
+        
+    }
+}

a347ea9164b2fc53206e4c0a92f0ddf4f7c9abb8
yu myeong cheol <mspringmspring@naver.com>
2020-07-06 23:20:10 +0900

    [CREATE] <auth/facebook> facebook passport

---
+++ SNS/src/auth/facebook.ts
@@ -0,0 +1,36 @@
+import FaceBook from "passport-facebook";
+import passport from "passport";
+import path from 'path';
+import * as dotenv from "dotenv";
+import { User } from "../models/user";
+
+const FaceBookStrategy = FaceBook.Strategy
+dotenv.config({ path: path.join(__dirname, '../../.env') });
+
+passport.use(new FaceBookStrategy({
+    clientID: "2597531827227737",
+    clientSecret: "66ff19d5a549446628beb02640f95e15",
+    callbackURL: "http://localhost:8080/api/auth/facebook/callback",
+    profileFields: ['id', 'displayName', 'link', 'email']
+}, async function(accessToken, refreshToken, profile, cb) {
+    if(!await User.findOne({ where: { email: profile._json.email }})){
+        await User.create({ 
+            email: profile._json.email,
+            password: `${profile._json.id}${profile._json.email}`,
+            nickName: profile._json.name
+        });
+    }
+
+    return cb(null, profile);
+  }
+));
+
+passport.serializeUser(function(user, cb) {
+    cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+    cb(null, obj);
+});
+
+module.exports = passport;

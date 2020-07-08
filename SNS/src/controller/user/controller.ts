66002d6a9652d25451d04f7139326809bcfd9180
yu myeong cheol <mspringmspring@naver.com>
2020-07-08 23:23:14 +0900

    [ADD] <user/controller.ts> make nodemailer

--- SNS/src/controller/user/controller.ts
+++ SNS/src/controller/user/controller.ts
@@ -1,45 +1,67 @@
 import * as query from "./query";
 import { Request, Response } from "express";
+import { mailSender } from "./nodemailer";
 
-interface userType {
-    email: string;
-    password: string;
-    nickName: string;
-}
+interface type { (req: Request, res: Response): Promise<void> }
+interface userType { email: string, password: string, nickName: string }
 
-const findAllUser = async (req: Request, res: Response) => {
+const findAllUser: type = async (req, res) => {
     const allUser: object = await query.findAllUser();
     res.status(200).json({ message: 'findAllUser', allUser });
 }
 
-const showUser = async (req: Request, res: Response) => {
-    const user:object = await query.findOneUserById(req.params.email);
+const showUser: type = async (req, res) => {
+    const user: object = await query.findOneUserById(req.params.email);
     res.status(200).json({ message: 'findOneUser', email: user['email'], nickName: user['email'] });
 }
 
-const signUpUser = async (req: Request, res: Response) => {
-    const { email, password, nickName }: userType = req.body;
-    
-    if (await query.findOneUserById(email)) 
-        throw new Error('email already exist');
+const signUpUser: type = async (req, res) => {
+    try {
+        const email: string = req.body.email;
 
-    await query.userCreate(email, password, nickName);
-    res.status(200).json({ message: 'signUp success' }).end();
+        const jbRandom = Math.random();
+        const code = Math.floor(jbRandom * 100000);
+
+        if (await query.findOneUserById(email))
+            throw new Error('email already exist');
+
+        const emailParam = {
+            toEmail: email,
+            subject: 'SNS Cheak Email',
+            text: `${code}`
+        };
+
+        mailSender.sendGmail(emailParam);
+        res.json({ message: 'Send Email Verification Code', code: `${code}` });
+    } catch (e) {
+        res.status(404).json({ message: e.message }).end();
+    }
 }
 
-const userUpdate = async (req: Request, res: Response) => {
-    // const { password, nickName }: userType = req.body;
+const checkEmail: type = async (req, res) => {
+    try {
+        const { email, password, nickName }: userType = req.body;
 
-    // if (password) await query.passwordUpdate(password, req.params.email);
-    // if (nickName) await query.nickNameUpdate(nickName, req.params.email);
-    // res.status(200).json({ message: 'user update' }).end();
+        await query.userCreate(email, password, nickName);
+        res.status(200).json({ message: 'signUp success' }).end();
+    } catch (e) {
+        res.status(404).json({ message: e.message }).end();
+    }
 }
 
-const userDelete = async (req: Request, res: Response) => {
+const userUpdate: type = async (req, res) => {
+    const { password, nickName }: userType = req.body;
+
+    if (password) await query.passwordUpdate(password, req.params.email);
+    if (nickName) await query.nickNameUpdate(nickName, req.params.email);
+    res.status(200).json({ message: 'user update' }).end();
+}
+
+const userDelete: type = async (req, res) => {
     const password: string = req.body.password;
     const user: object = await query.findOneUserById(req.params.email);
-    
-    if(user['password'] === password)
+
+    if (user['password'] === password)
         await query.deleteUser(req.params.email);
 
     res.status(200).json({ message: 'Delete Successful' });
@@ -48,19 +70,20 @@ const userDelete = async (req: Request, res: Response) => {
 const checkPermission = async (req: Request, res: Response, next): Promise<any> => {
     try {
         const user: object = await query.findOneUserById(req.params.email);
-        
+
         if (!req['decoded'] || user['id'] != req["decoded"].id)
-            res.json({ message: 'You don\'t have permission' }); 
+            res.json({ message: 'You don\'t have permission' });
         else next();
     } catch (e) {
         res.status(404).json({ message: e.message }).end();
     }
 }
 
-export { 
+export {
     findAllUser,
     showUser,
     signUpUser,
+    checkEmail,
     userUpdate,
     userDelete,
     checkPermission

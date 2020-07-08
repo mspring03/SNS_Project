f4e5944b0e25082fdb7c76e4ad226ae3225b69bd
yu myeong cheol <mspringmspring@naver.com>
2020-07-08 23:31:10 +0900

    [MODIFY] <user/nodemailer.ts> modify

--- SNS/src/controller/user/nodemailer.ts
+++ SNS/src/controller/user/nodemailer.ts
@@ -6,33 +6,34 @@ import * as dotenv from "dotenv";
 dotenv.config({ path: path.join(__dirname, '../../.env') });
 
 export const mailSender = {
-    sendGmail : function(param){
+    sendGmail: function (param) {
         const transporter = nodemailer.createTransport({
             service: 'gmail'
-            ,prot : 587
-            ,host :'smtp.gmlail.com'
-            ,secure : false
-            ,requireTLS : true
+            , prot: 587
+            , host: 'smtp.gmlail.com'
+            , secure: false
+            , requireTLS: true
             , auth: {
-              user: process.env.Email,
-              pass: process.env.Email_Password
+                user: process.env.Email,
+                pass: process.env.Email_Password
             }
         });
+
         // 메일 옵션
         const mailOptions = {
-                from: process.env.Email,
-                to: param.toEmail, // 수신할 이메일
-                subject: param.subject, // 메일 제목
-                text: param.text // 메일 내용
-            };
+            from: process.env.Email,
+            to: param.toEmail, // 수신할 이메일
+            subject: param.subject, // 메일 제목
+            text: param.text // 메일 내용
+        };
 
-        transporter.sendMail(mailOptions, function(error, info){
+        transporter.sendMail(mailOptions, function (error, info) {
             if (error) {
                 console.log(error);
             } else {
                 console.log('Email sent: ' + info.response);
             }
         });
-        
+
     }
 }

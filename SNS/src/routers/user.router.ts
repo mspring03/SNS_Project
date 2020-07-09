1d452201ce9d48fdadbda5960b752f070cd7efc5
yu myeong cheol <mspringmspring@naver.com>
2020-07-10 05:26:30 +0900

    [MODIFY] <routers/user.router.ts> modify router

--- SNS/src/routers/user.router.ts
+++ SNS/src/routers/user.router.ts
@@ -2,20 +2,23 @@ import express from "express";
 import * as userController from "../controller/user/controller";
 import * as middelware from "../middleware/errorHandler"
 import * as authMiddleware from "../middleware/jwt";
+import { use } from "passport";
 
 const router = express.Router();
 const findAllUserHanddler = middelware.tryCatchMiddleware.NotFound(userController.findAllUser);
 const showUserHanddler = middelware.tryCatchMiddleware.NotFound(userController.showUser);
 const signUpUserHanddler = middelware.tryCatchMiddleware.NotFound(userController.signUpUser);
+const checkEmailHanddler = middelware.tryCatchMiddleware.NotFound(userController.checkEmail);
 const userUpdateHanddler = middelware.tryCatchMiddleware.NotFound(userController.userUpdate);
 const userDeleteHanddler = middelware.tryCatchMiddleware.NotFound(userController.userDelete);
 const cheakPermission = userController.checkPermission;
 const accessToken = authMiddleware.accessToken;
 
 router.get("/", accessToken, findAllUserHanddler);
-router.get("/:userName", accessToken, showUserHanddler);
+router.get("/:email", accessToken, showUserHanddler);
 router.post("/", signUpUserHanddler);
-router.put("/:userName", accessToken, cheakPermission, userUpdateHanddler);
-router.delete("/:userName", accessToken, cheakPermission, userDeleteHanddler);
+router.post("/check", checkEmailHanddler);
+router.put("/:email", accessToken, cheakPermission, userUpdateHanddler);
+router.delete("/:email", accessToken, cheakPermission, userDeleteHanddler);
 
 export default router;

8866b76e30eb356baa5d026e8b41f79dba31d9d9
yu myeong cheol <mspringmspring@naver.com>
2020-07-13 21:46:19 +0900

    [ADD] <user/controller.ts> password encryption

--- SNS/src/controller/user/controller.ts
+++ SNS/src/controller/user/controller.ts
@@ -1,6 +1,6 @@
 import * as query from "./query";
 import { Request, Response } from "express";
-import { mailSender } from "./nodemailer";
+import { mailSender } from "../../middleware/nodemailer";
 
 interface type { (req: Request, res: Response): Promise<void> }
 interface userType { email: string, password: string, nickName: string }
@@ -16,44 +16,37 @@ const showUser: type = async (req, res) => {
 }
 
 const signUpUser: type = async (req, res) => {
-    try {
-        const email: string = req.body.email;
+    const email: string = req.body.email;
+    const code = Math.floor(Math.random() * 100000);
 
-        const jbRandom = Math.random();
-        const code = Math.floor(jbRandom * 100000);
+    if (await query.findOneUserById(email))
+        throw new Error('email already exist');
 
-        if (await query.findOneUserById(email))
-            throw new Error('email already exist');
+    const emailParam = {
+        toEmail: email,
+        subject: 'SNS Cheak Email',
+        text: `${code}`
+    };
 
-        const emailParam = {
-            toEmail: email,
-            subject: 'SNS Cheak Email',
-            text: `${code}`
-        };
-
-        mailSender.sendGmail(emailParam);
-        res.status(200).json({ message: 'Send Email Verification Code', code: `${code}` });
-    } catch (e) {
-        res.status(404).json({ message: e.message }).end();
-    }
+    mailSender.sendGmail(emailParam);
+    res.status(200).json({ message: 'Send Email Verification Code', code: `${code}` });
 }
 
 const checkEmail: type = async (req, res) => {
-    try {
-        const { email, password, nickName }: userType = req.body;
+    const { email, password, nickName }: userType = req.body;
+    const hashingPW = await query.passwordHashing(password);
 
-        await query.userCreate(email, password, nickName);
-        res.status(200).json({ message: 'signUp success' }).end();
-    } catch (e) {
-        res.status(404).json({ message: e.message }).end();
-    }
+    await query.userCreate(email, hashingPW, nickName);
+    res.status(200).json({ message: 'signUp success' }).end();
 }
 
 const userUpdate: type = async (req, res) => {
     const { password, nickName }: userType = req.body;
+    const hashingPW = await query.passwordHashing(password);
 
-    if (password) await query.passwordUpdate(password, req.params.email);
+    if (password) await query.passwordUpdate(hashingPW, req.params.email);
     if (nickName) await query.nickNameUpdate(nickName, req.params.email);
+
     res.status(200).json({ message: 'user update' }).end();
 }
 
@@ -61,8 +54,9 @@ const userDelete: type = async (req, res) => {
     const password: string = req.body.password;
     const user: object = await query.findOneUserById(req.params.email);
 
-    if (user['password'] === password)
+    if (await query.passwordCompare(password, user['password']))
         await query.deleteUser(req.params.email);
+    else throw new Error('wrong password');
 
     res.status(200).json({ message: 'Delete Successful' });
 }

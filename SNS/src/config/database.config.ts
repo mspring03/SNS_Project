eefd87880c028630d1e7195f33f348d862407aaf
yu myeong cheol <mspringmspring@naver.com>
2020-07-16 11:02:15 +0900

    [MODIFY] <config/database.config.ts>

--- SNS/src/config/database.config.ts
+++ SNS/src/config/database.config.ts
@@ -3,14 +3,14 @@ import path from 'path';
 dotenv.config({ path: path.join(__dirname, '../../.env') });
 
 const Config = {
-    test_mode: false,
-    database: {
-      host: process.env.HOST || "",
-      port: process.env.PORT || "",
-      user: process.env.USER || "",
-      password: process.env.PASS || "",
-      name: process.env.NAME || "",
-    },
-  };
-  
-  export default Config;
+  test_mode: false,
+  database: {
+    host: process.env.HOST || "",
+    port: process.env.PORT,
+    user: process.env.USER || "",
+    password: process.env.PASS || "",
+    name: process.env.NAME || "",
+  },
+};
+
+export default Config;

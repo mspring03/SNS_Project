3177ab0a487c6854a916e46fc1f04404408a087e
yu myeong cheol <mspringmspring@naver.com>
2020-09-26 23:32:20 +0900

    [MODIFY] <middleware/errorHandler.ts> modify class

--- SNS/src/middleware/errorHandler.ts
+++ SNS/src/middleware/errorHandler.ts
@@ -1,27 +1,16 @@
 import { Request, Response } from "express";
 
 interface handlerFunc {
-    (req: Request, res: Response): void ;
-} 
+    (req: Request, res: Response): void;
+}
 
 export class tryCatchMiddleware {
-
-    static NotFound = (myFunc: handlerFunc):handlerFunc => {
+    static Error = (myFunc: handlerFunc): handlerFunc => {
         return async (req: Request, res: Response) => {
             try {
                 await myFunc(req, res);
             } catch (e) {
-                res.status(404).json({ message: e.message }).end();
-            }
-        }
-    }
-
-    static ServerError = (myFunc: handlerFunc):handlerFunc => {
-        return async (req: Request, res: Response) => {
-            try {
-                await myFunc(req, res);
-            } catch (e) {
-                res.status(500).json({ message: e.message }).end();
+                res.status(e.statusCode || 500).json({ message: e.message });
             }
         }
     }

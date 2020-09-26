d1ef24c41f72a2dea04c9f536d04cef8c64f4fec
yu myeong cheol <mspringmspring@naver.com>
2020-09-26 23:33:49 +0900

    [ADD] <routers/index.ts>

--- SNS/src/routers/index.ts
+++ SNS/src/routers/index.ts
@@ -2,11 +2,13 @@ import express from "express";
 import authRouter from "./auth.router";
 import userRouter from "./user.router";
 import postRouter from "./post.router";
+import friendRouter from "./friend.router"
 
 const router = express.Router();
 
 router.use("/auth", authRouter);
 router.use("/user", userRouter);
 router.use("/post", postRouter);
+router.use("/friend", friendRouter)
 
 export default router;

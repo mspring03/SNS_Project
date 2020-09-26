864cc0194294482e63fed781740afc2be3238487
yu myeong cheol <mspringmspring@naver.com>
2020-09-26 23:29:55 +0900

    [CREATE] <post/controller.ts> func posting, likeUp, likeDown

--- SNS/src/controller/post/controller.ts
+++ SNS/src/controller/post/controller.ts
@@ -1,12 +1,10 @@
 import * as query from "./query";
 import { Request, Response } from "express";
-import { User } from "../../models/user";
-import { Post } from "../../models/post"
 
 interface type { (req: Request, res: Response): Promise<void> }
 
 const registerPost: type = async (req, res) => {
-    const description = req.body;
+    const description: string = req.body.description;
     const userId = req['decoded'].id;
     const imgArray = req['files'];
     await query.postUpload(userId, imgArray, description);
@@ -15,77 +13,40 @@ const registerPost: type = async (req, res) => {
 }
 
 const deletePost: type = async (req, res) => {
+    const postId = req.body.postId;
+    await query.postDelete(postId);
 
+    res.status(200).json({ message: "success" });
 }
 
-//오늘 1커밋 하세영
+const posting: type = async (req, res) => {
+    const userId = req['decoded'].id;
+    const page = req.body.page;
+    const posts = await query.posting(userId, page);
 
+    res.status(200).json({ message: "success", posts });
+}
 
+const likeUp: type = async (req, res) => {
+    const { postId, userId } = req.body;
 
+    await query.likeUp(postId, userId);
 
+    res.status(200).json({ message: "success" });
+}
 
+const likeDown: type = async (req, res) => {
+    const { postId, userId } = req.body;
 
+    await query.likeDown(postId, userId);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const test: type = async (req, res) => {
-    try {
-        // const user: any = await User.findOne({
-        //     include: [
-        //         {
-        //             model: Post,
-        //             required: false,
-        //             attributes: ['id', 'description', 'like']
-        //         }],
-        //     where: {
-        //         id: "3"
-        //     },
-        //     raw: true,
-        // })
-        console.log(req["decoded"].id);
-
-        // const user: any = await User.findAll({
-        //     where: ({ id: 3 }),
-        //     include: [
-        //         {
-        //             model: Post,
-        //             required: false,
-        //             attributes: ['id', 'description', 'like']
-        //         }
-        //     ],
-        //     raw: true
-        // })
-
-        await query.postUpload(3, req['files'], "1");
-        //console.log(req['files']);
-        res.json({ a: "success" }).end();
-        // console.log(user);
-
-
-    } catch (err) {
-        console.log(err);
-
-    }
+    res.status(200).json({ message: "success" });
 }
 
 export {
     registerPost,
-    test
+    deletePost,
+    posting,
+    likeUp,
+    likeDown
 }

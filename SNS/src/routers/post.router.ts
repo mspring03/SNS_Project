b73c172cf6c432e3a2fbaa172edb44dcaedfc21c
yu myeong cheol <mspringmspring@naver.com>
2020-09-26 23:34:04 +0900

    [MODIFY] <routers/post.router.ts>

--- SNS/src/routers/post.router.ts
+++ SNS/src/routers/post.router.ts
@@ -1,15 +1,21 @@
-import express from "express";
 import * as postController from "../controller/post/controller";
-import * as middelware from "../middleware/errorHandler"
-import * as authMiddleware from "../middleware/jwt";
+import * as middleware from "../middleware/errorHandler"
+import * as authMiddleware from "../middleware/tokenDecoder";
 import router from "./auth.router";
 import * as fileManagement from "../middleware/fileManagement";
 
-const registerPostHanddler = middelware.tryCatchMiddleware.NotFound(postController.registerPost);
-const deletePostHanddler = middelware.tryCatchMiddleware.NotFound(postController.deletePost);
+const registerPostHanddler = middleware.tryCatchMiddleware.Error(postController.registerPost);
+const deletePostHanddler = middleware.tryCatchMiddleware.Error(postController.deletePost);
+const postingHanddler = middleware.tryCatchMiddleware.Error(postController.posting);
+const likeUpHanddler = middleware.tryCatchMiddleware.Error(postController.likeUp);
+const likeDownHanddler = middleware.tryCatchMiddleware.Error(postController.likeDown);
+
 const accessToken = authMiddleware.accessToken;
 
 router.post("/", accessToken, fileManagement.postImgUpload, registerPostHanddler);
 router.delete("/", accessToken, fileManagement.postImgDelete, deletePostHanddler);
+router.post("/posting", accessToken, postingHanddler);
+router.post("likeUp", accessToken, likeUpHanddler);
+router.post("likeDown", accessToken, likeDownHanddler);
 
 export default router;

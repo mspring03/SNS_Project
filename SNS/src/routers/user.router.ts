3da985624fbd9b6c5c903f17dcace1e0dd760cd0
yu myeong cheol <mspringmspring@naver.com>
2020-09-26 23:34:22 +0900

    [MODIFY] <routers/user.router.ts>

--- SNS/src/routers/user.router.ts
+++ SNS/src/routers/user.router.ts
@@ -1,17 +1,17 @@
 import express from "express";
 import * as userController from "../controller/user/controller";
 import * as middelware from "../middleware/errorHandler"
-import * as authMiddleware from "../middleware/jwt";
+import * as authMiddleware from "../middleware/tokenDecoder";
 import * as fileManagement from "../middleware/fileManagement";
 
 const router = express.Router();
-const findAllUserHanddler = middelware.tryCatchMiddleware.NotFound(userController.findAllUser);
-const showUserHanddler = middelware.tryCatchMiddleware.NotFound(userController.showUser);
-const emailSenderHanddler = middelware.tryCatchMiddleware.NotFound(userController.emailSender);
-const signUpHanddler = middelware.tryCatchMiddleware.NotFound(userController.signUp);
-const passwordUpdateHanddler = middelware.tryCatchMiddleware.NotFound(userController.passwordUpdate);
-const userUpdateHanddler = middelware.tryCatchMiddleware.NotFound(userController.userUpdate);
-const userDeleteHanddler = middelware.tryCatchMiddleware.NotFound(userController.userDelete);
+const findAllUserHanddler = middelware.tryCatchMiddleware.Error(userController.findAllUser);
+const showUserHanddler = middelware.tryCatchMiddleware.Error(userController.showUser);
+const emailSenderHanddler = middelware.tryCatchMiddleware.Error(userController.emailSender);
+const signUpHanddler = middelware.tryCatchMiddleware.Error(userController.signUp);
+const passwordUpdateHanddler = middelware.tryCatchMiddleware.Error(userController.passwordUpdate);
+const userUpdateHanddler = middelware.tryCatchMiddleware.Error(userController.userUpdate);
+const userDeleteHanddler = middelware.tryCatchMiddleware.Error(userController.userDelete);
 const cheakPermission = userController.checkPermission;
 const accessToken = authMiddleware.accessToken;
 

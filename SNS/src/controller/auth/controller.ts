9936104841c670b24332b67678d94743244ca3bf
yu myeong cheol <mspringmspring@naver.com>
2020-09-26 23:23:55 +0900

    [MODIFY] <auth/controller.ts> throw error class

--- SNS/src/controller/auth/controller.ts
+++ SNS/src/controller/auth/controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import * as userQuery from "./query";
 import * as token from "./mkToken";
+import HTTPError from "../../exception/exception";
 
 interface type { (req: Request, res: Response): void; }
 
@@ -12,14 +13,14 @@ const signInUser: type = async (req, res) => {
     const user: object = await userQuery.findUserByEmail(email);
     const firstLogin: boolean = !Boolean(user['token']);
 
-    if (!user) throw new Error('Incorrect email or password');
+    if (!user) throw new HTTPError(404, 'Incorrect email or password');
 
     if (await userQuery.passwordCompare(password, user['password'])) {
         const token: string = await mktoken(req, user);
         const refreshtoken: string = await mkRefreshtoken(req, user);
         await userQuery.tokenUpdate(user['email'], token, refreshtoken);
         res.status(200).json({ message: 'signIn success', token, refreshtoken, firstLogin }).end();
-    } else throw new Error('Incorrect email or password');
+    } else throw new HTTPError(404, 'Incorrect email or password');
 }
 
 const tokenValidation: type = async (req, res) => {
@@ -30,7 +31,7 @@ const me: type = async (req, res) => {
     const accesstoken: any = req.headers['x-access-token'];
 
     const user: object = await userQuery.findUserByToken(accesstoken);
-    if (!user) throw new Error('not found user');
+    if (!user) throw new HTTPError(404, 'not found user');
     res.status(200).json({ message: 'user return', user });
 }
 
@@ -43,7 +44,7 @@ const refresh: type = async (req, res) => {
         const token: string = await mktoken(req, user);
         await userQuery.tokenUpdate(user['email'], token, refreshtoken);
         res.status(200).json({ message: 'token refresh', token, refreshtoken }).end();
-    } else throw new Error('Another token');
+    } else throw new HTTPError(404, 'Another token');
 }
 
 const passport = async (req, res) => {
@@ -52,7 +53,6 @@ const passport = async (req, res) => {
     const token: string = await mktoken(req, user);
     const refreshtoken: string = await mkRefreshtoken(req, user);
     console.log(firstLogin);
-
 
     await userQuery.tokenUpdate(user['email'], token, refreshtoken);
     const u: object = await userQuery.findUserByEmail(req.user.emails[0].value);
